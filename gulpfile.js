6399de4dc835af68cbbeb9f786edec413d63405a
Anastasia Abrosimova <abroana19@gmail.com>
2016-09-26 02:39:26 +0300

    Pixel perfect
    ... not yet

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    cssimport = require('postcss-import'),
     browserify = require('gulp-browserify');
 
 gulp.task('js', function() {
@@ -11,11 +15,20 @@ gulp.task('js', function() {
 });
 
 gulp.task('css', function() {
+  var processors = [
+        cssimport({from: 'app/css/style.css'}),
+        autoprefixer({browsers: ['> 1%']})
+      ];
   gulp.src('app/css/style.css')
+    .pipe(postcss(processors))
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.watch('app/**', function(event) {
-  gulp.run('js');
-  gulp.run('css');
-});
+/*gulp.task('html', function() {
+  gulp.src('app/index.html')
+    .pipe(gulp.dest('dest'));
+});*/
+
+gulp.watch('app/**', ['js' , 'css']);
+
+gulp.task('default', ['js' , 'css']);
